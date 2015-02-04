74079607011322629e07688138a8917242212a91
jspencerhopkins <jspencerhopkins@gmail.com>
2015-02-03 20:38:57 -0500

    Add reviewer column to reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203202713) do
+ActiveRecord::Schema.define(version: 20150204013746) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150203202713) do
     t.integer  "restaurant_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "reviewer"
   end
 
   create_table "users", force: :cascade do |t|
