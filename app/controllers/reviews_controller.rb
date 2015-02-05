b10ac5998218e69acee451f170f2d62e841cb688
jspencerhopkins <jspencerhopkins@gmail.com>
2015-02-05 11:48:03 -0500

    Changes made during lecture part 1

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,8 @@ class ReviewsController < ApplicationController
 
 	def create
 		@restaurant = Restaurant.find(params[:restaurant_id])
-		@review = @restaurant.reviews.create(review_params)
+		@review = @restaurant.reviews.create(params_including_user_id)
+		flash[:notice] = "Woohoo new review!"
 		redirect_to @restaurant
 	end	
 
@@ -13,10 +14,13 @@ class ReviewsController < ApplicationController
     redirect_to restaurant_path(@restaurant)
   end
 
+  def params_including_user_id
+  	review_params.merge(user_id: session[:user_id])
+  end
 
 	private
 		def review_params
-			params.require(:review).permit(:user_id, :comment, :rating, :recommend)
+			params.require(:review).permit(:comment, :rating, :recommend)
 		end
 
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+	def create
+		# my params has is 
+		params[:email]
+		user = User.where(email: params[:email]).first
+		
+		if user
+			session[:user_id] = user.id
+			flash[:notice] = "Welcome back, #{user.name}!"
+			redirect_to root_url
+		else
+			flash[:alert] = "You type like a dumbass"
+			redirect_to login_url
+		end
+
+	end
+
+end
